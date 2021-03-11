3fea9aad5af297cde7c38753b97afe547436a050
tezc <tezc@users.noreply.github.com>
2021-03-11 09:32:16 +0000

    [skip ci] bump version to v0.0.11-latest

--- java/src/main/java/resql/Resql.java
+++ java/src/main/java/resql/Resql.java
@@ -26,7 +26,7 @@ package resql;
 
 public interface Resql extends AutoCloseable {
 
-    String VERSION = "0.0.10";
+    String VERSION = "0.0.11-latest";
 
     /**
      * Prepare Statement
--- util/cli/resql_cli.c
+++ util/cli/resql_cli.c
@@ -47,7 +47,7 @@ struct resql_cli
     int count;
 };
 
-#define RESQL_CLI_VERSION "0.0.10"
+#define RESQL_CLI_VERSION "0.0.11-latest"
 
 static struct resql_cli s_cli;
 static struct resql *client;
