4617f47128a8700a856acf9477450669911aa632
tezc <tezc@users.noreply.github.com>
2021-03-11 03:23:03 +0000

    bump version to 0.0.4

--- src/rs.h
+++ src/rs.h
@@ -27,7 +27,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define RS_VERSION  "0.0.4-latest"
+#define RS_VERSION  "0.0.4"
 
 #ifdef RS_COMPILER_GIT_BRANCH
     #define RS_GIT_BRANCH RS_COMPILER_GIT_BRANCH
