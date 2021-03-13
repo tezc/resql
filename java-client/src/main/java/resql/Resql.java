6837b906200b99206b1061ca0ae95dcfa16e0da9
tezc <tezc@users.noreply.github.com>
2021-03-13 10:41:32 +0000

    [skip ci] bump version to v0.0.15

--- java-client/src/main/java/resql/Resql.java
+++ java-client/src/main/java/resql/Resql.java
@@ -26,7 +26,7 @@ package resql;
 
 public interface Resql extends AutoCloseable {
 
-    String VERSION = "0.0.15-latest";
+    String VERSION = "0.0.15";
 
     /**
      * Prepare Statement
--- src/rs.h
+++ src/rs.h
@@ -27,7 +27,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define RS_VERSION  "0.0.15-latest"
+#define RS_VERSION  "0.0.15"
 
 #ifdef RS_COMPILER_GIT_BRANCH
     #define RS_GIT_BRANCH RS_COMPILER_GIT_BRANCH
