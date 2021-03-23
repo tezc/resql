a31821ea3ec126b601bbb0c43660cc0fe9ff5a3f
tezc <tezc.vx@gmail.com>
2021-03-23 04:57:37 +0300

    debug

--- c-client/resql.c
+++ c-client/resql.c
@@ -67,6 +67,16 @@ typedef int sc_sock_int;
     #include <time.h>
 #endif
 
+static const char* date()
+{
+    char text[100];
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+
+    strftime(text, sizeof(text)-1, "%d %m %Y %H:%M", t);
+    printf("[%s][before log] \n", text);
+}
+
 static uint64_t sc_time_mono_ms()
 {
 #if defined(_WIN32) || defined(_WIN64)
@@ -1849,6 +1859,7 @@ int resql_recv_connect_resp(struct resql *c)
 retry:
     rc = sc_sock_recv(&c->sock, sc_buf_wbuf(resp), sc_buf_quota(resp), 0);
     if (rc < 0) {
+        date();
         resql_err(c, "sock recv failure : %s ", strerror(errno));
         return RESQL_ERROR;
     }
@@ -1920,6 +1931,7 @@ int resql_connect(struct resql *c)
 
     sc_sock_init(&c->sock, 0, false, family);
 
+    date();
     printf("cclient is trying to connect to %s \n", uri->str);
     rc = sc_sock_connect(&c->sock, host, uri->port, c->source_addr,
                          c->source_port);
@@ -1944,6 +1956,8 @@ retry:
         rc = RESQL_ERROR;
         goto sock_error;
     }
+
+    printf("cclient: connect() succeed. \n");
 
     rc = sc_sock_set_blocking(&c->sock, true);
     if (rc != SC_SOCK_OK) {
@@ -2207,7 +2221,7 @@ retry_recv:
     n = sc_sock_recv(&c->sock, sc_buf_wbuf(&c->resp), sc_buf_quota(&c->resp),
                      0);
     if (n <= 0) {
-        printf("cclient socket recv :%s \n", sc_sock_error(&c->sock));
+        printf("cclient socket recv(%d) :%s \n", n, sc_sock_error(&c->sock));
         c->connected = false;
         sc_sock_term(&c->sock);
         goto retry_op;
