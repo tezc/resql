1acacaa78d3d4f40f901b0d5060914afa9b6408f
tezc <tezc.vx@gmail.com>
2021-05-13 16:48:10 +0300

    sc.h update to_pow2 function

--- test/page_test.c
+++ test/page_test.c
@@ -98,27 +98,23 @@ static void page_expand_test(void)
 	uint32_t size;
 
 	page_init(&page, test_tmp_page0, -1, prev_index);
-	sc_log_info("Page init - 1 \n");
 
 	for (int i = 0; i < 4000; i++) {
 		page_create_entry(&page, i, i, i, i, data, strlen(data) + 1);
-		sc_log_info("Page create entry - 1 \n");
 
 		if (i % 2 == 0) {
 			page_fsync(&page, 5000 + 500);
 		}
-		sc_log_info("Page fsync - 1 \n");
 	}
 
-	sc_log_info("Page quota \n");
+
 	size = page_quota(&page);
 	rs_assert(size < 50000000);
 
 	rs_assert(page_reserve(&page, 50000000) == RS_OK);
 	rs_assert(page_quota(&page) >= 50000000);
-	sc_log_info("Page reserve \n");
-	rs_assert(page_reserve(&page, 2 * 1024 * 1024 * 1024ull) != RS_OK);
-	sc_log_info("Page reserve 2\n");
+
+	rs_assert(page_reserve(&page, UINT32_C(2 * 1024 * 1024 * 1024)) != RS_OK);
 
 	for (uint64_t i = 0; i < 4000; i++) {
 		entry = page_entry_at(&page, prev_index + 1 + i);
