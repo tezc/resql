3d5179e42700d9b25ca400aebaa8832077024ff7
tezc <tezc.vx@gmail.com>
2021-05-13 15:21:23 +0300

    do not set socket buffers in C and Java client

--- test/snapshot_test.c
+++ test/snapshot_test.c
@@ -219,7 +219,7 @@ static void snapshot_two()
 
 	test_server_add_auto();
 	test_server_destroy_leader();
-	sleep(15);
+	sleep(30);
 
 	c = test_client_create();
 	resql_put_sql(c, "Select count(*) from snapshot;");
