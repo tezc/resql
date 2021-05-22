7d41b3edbc323afda2cf437e91b22025f139634c
yasemill <yasemill17@gmail.com>
2021-05-22 17:32:25 +0300

    add default scheme and port to benchmark urls

--- util/benchmark/resql_benchmark.c
+++ util/benchmark/resql_benchmark.c
@@ -70,6 +70,171 @@ _Atomic uint64_t ops;
 _Atomic uint64_t end_ts;
 _Atomic uint64_t start_ts;
 
+struct sc_uri {
+	const char *str; // Full string
+	const char *scheme;
+	const char *host;
+	const char *userinfo;
+	const char *port;
+	const char *path;
+	const char *query;
+	const char *fragment;
+
+	char buf[];
+};
+
+static struct sc_uri *sc_uri_create(const char *str)
+{
+	const char *s1 = "%.*s%.*s%.*s%.*s%.*s%.*s%.*s%.*s";
+	const char *s2 = "%.*s%c%.*s%c%.*s%c%.*s%c%.*s%c%.*s%c%.*s%c";
+	const char *authority = "//";
+
+	int diff, ret;
+	unsigned long val;
+	size_t full_len, parts_len;
+	size_t scheme_len, authority_len = 0, userinfo_len = 0;
+	size_t host_len = 0, port_len = 0, path_len;
+	size_t query_len = 0, fragment_len = 0;
+
+	char *scheme, *userinfo = "", *host = "", *port = "";
+	char *path, *query = "", *fragment = "";
+	char *ptr, *dest, *parse_end;
+	char *pos = (char *) str;
+	struct sc_uri *uri;
+
+	if (str == NULL || (ptr = strstr(pos, ":")) == NULL) {
+		return NULL;
+	}
+
+	scheme = pos;
+	scheme_len = ptr - str + 1;
+	pos = ptr + 1;
+
+	if (*pos == '/' && *(pos + 1) == '/') {
+		authority_len = 2;
+		pos += authority_len;
+
+		ptr = strchr(pos, '@');
+		if (ptr != NULL) {
+			userinfo = pos;
+			userinfo_len = ptr - pos + strlen("@");
+			pos = ptr + 1;
+		}
+
+		ptr = pos + strcspn(pos, *pos == '[' ? "]" : ":/?#");
+		host = pos;
+		host_len = ptr - pos + (*host == '[');
+		pos = host + host_len;
+
+		if (*host == '[' && *(host + host_len - 1) != ']') {
+			return NULL;
+		}
+
+		ptr = strchr(pos, ':');
+		if (ptr != NULL) {
+			if (*(ptr + 1) == '\0') {
+				return NULL;
+			}
+
+			errno = 0;
+			val = strtoul(ptr + 1, &parse_end, 10);
+			if (errno != 0 || val > 65536) {
+				return NULL;
+			}
+
+			port = ptr;
+			port_len = parse_end - ptr;
+			pos = port + port_len;
+		}
+	}
+
+	path = pos;
+	path_len = strcspn(path, "?#");
+	pos = path + path_len;
+
+	ptr = strchr(pos, '?');
+	if (ptr != NULL) {
+		query = ptr;
+		query_len = strcspn(query, "#");
+		pos = query + query_len;
+	}
+
+	if (*pos == '#') {
+		fragment = pos;
+		fragment_len = strlen(pos);
+	}
+
+	full_len = scheme_len + authority_len + userinfo_len + host_len +
+		   port_len + path_len + query_len + fragment_len + 1;
+
+	parts_len = full_len - authority_len;
+	parts_len += 7; // NULL characters for each part.
+	parts_len -= (scheme_len != 0);
+	parts_len -= (userinfo_len != 0);
+	parts_len -= (port_len != 0);
+	parts_len -= (query_len != 0);
+	parts_len -= (fragment_len != 0);
+
+	uri = malloc(sizeof(*uri) + parts_len + full_len);
+	if (uri == NULL) {
+		return NULL;
+	}
+
+	ret = snprintf(uri->buf, full_len, s1, scheme_len, scheme,
+		       authority_len, authority, userinfo_len, userinfo,
+		       host_len, host, port_len, port, path_len, path,
+		       query_len, query, fragment_len, fragment);
+	if (ret < 0 || (size_t) ret != full_len - 1) {
+		goto error;
+	}
+
+	dest = uri->buf + strlen(uri->buf) + 1;
+
+	scheme_len -= (scheme_len != 0);     // Skip ":"
+	userinfo_len -= (userinfo_len != 0); // Skip "@"
+	diff = port_len != 0;
+	port_len -= diff; // Skip ":"
+	port += diff;	  // Skip ":"
+	diff = (query_len != 0);
+	query_len -= diff; // Skip "?"
+	query += diff;	   // Skip "?"
+	diff = (fragment_len != 0);
+	fragment_len -= diff; // Skip "#"
+	fragment += diff;     // Skip "#"
+
+	ret = sprintf(dest, s2, scheme_len, scheme, 0, userinfo_len, userinfo,
+		      0, host_len, host, 0, port_len, port, 0, path_len, path,
+		      0, query_len, query, 0, fragment_len, fragment, 0);
+	if (ret < 0 || (size_t) ret != parts_len - 1) {
+		goto error;
+	}
+
+	uri->str = uri->buf;
+	uri->scheme = dest;
+	uri->userinfo = dest + scheme_len + 1;
+	uri->host = uri->userinfo + userinfo_len + 1;
+	uri->port = uri->host + host_len + 1;
+	uri->path = uri->port + port_len + 1;
+	uri->query = uri->path + path_len + 1;
+	uri->fragment = uri->query + query_len + 1;
+
+	return uri;
+
+error:
+	free(uri);
+	return NULL;
+}
+
+static void sc_uri_destroy(struct sc_uri **uri)
+{
+	if (uri == NULL || *uri == NULL) {
+		return;
+	}
+
+	free(*uri);
+	*uri = NULL;
+}
+
 struct resql_benchmark {
 	char *url;
 	char *cluster_name;
@@ -88,14 +253,14 @@ struct resql_benchmark {
 	pthread_mutex_t mtx;
 };
 
-#define RESQL_BENCHMARK_VERSION "0.1.4-latest"
+#define RESQL_BENCHMARK_VERSION "0.1.3-latest"
 
 static struct resql_benchmark bench;
 
 static void resql_benchmark_usage()
 {
 	printf("\n resql-benchmark version : %s \n\n", RESQL_BENCHMARK_VERSION);
-	printf(" -u=<uri>      --uri=<uri>         ex: --uri=tcp://127.0.0.1:7600               \n"
+	printf(" -u=<uri>      --uri=<uri>         ex: --uri=127.0.0.1:7600                     \n"
 	       " -b=<count>    --batch=<count>     default is 1, e.g 5 inserts/selects in one op\n"
 	       " -i=<count>    --initial=<count>   initial table size                           \n"
 	       " -n=<name>     --name=<name>       cluster name, default is 'cluster'           \n"
@@ -221,9 +386,42 @@ void resql_benchmark_conf(struct resql_benchmark *b, int argc, char *argv[])
 			}
 			break;
 
-		case 'u':
+		case 'u': {
+			char tmp[2048] = {0};
+			struct sc_uri *uri = NULL;
+
+			if (strlen(val) > 2000) {
+				printf("Too long : %s \n", val);
+				exit(1);
+			}
+
 			free(b->url);
-			b->url = strdup(val);
+			strcpy(tmp, val);
+
+			uri = sc_uri_create(val);
+			if (!uri || strcmp(uri->scheme, "tcp") != 0) {
+				// Try to add scheme, 'tcp' as default
+				*tmp = '\0';
+				strcpy(tmp, "tcp://");
+				strcat(tmp, val);
+
+				uri = sc_uri_create(tmp);
+				if (!uri) {
+					printf("Cannot parse url : %s \n", val);
+					exit(1);
+				}
+			}
+
+			// Check if uri has port, add 7600 as default
+			if (*uri->port == '\0') {
+				*tmp = '\0';
+				strcpy(tmp, uri->str);
+				strcat(tmp, ":7600");
+			}
+
+			sc_uri_destroy(&uri);
+			b->url = strdup(tmp);
+		}
 			break;
 
 		case 'h':
@@ -771,4 +969,5 @@ int main(int argc, char **argv)
 
 	return 0;
 }
+
 
